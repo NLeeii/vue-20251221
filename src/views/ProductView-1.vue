b8284775b3ea60930993be9189c11421289327aa
NLeeii <niniabc920405@gmail.com>
2025-12-21 15:23:13 +0800

    使用axios取得資料

--- src/views/ProductView-1.vue
+++ src/views/ProductView-1.vue
@@ -1,52 +1,74 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue'
-const productList = ref([
-  {
-    id: 2,
-    title: 'Mens Casual Premium Slim Fit T-Shirts ',
-    price: 22.3,
-    description: 'Slim-fitting style',
-    category: "men's clothing",
-    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
-    rating: {
-      rate: 4.1,
-      count: 259,
-    },
-  },
-  {
-    id: 4,
-    title: 'Mens Casual Slim Fit',
-    price: 15.99,
-    description: 'The color could be slightly',
-    category: "men's clothing",
-    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png',
-    rating: {
-      rate: 2.1,
-      count: 430,
-    },
-  },
-])
+// const productList = ref([
+//   {
+//     id: 2,
+//     title: 'Mens Casual Premium Slim Fit T-Shirts ',
+//     price: 22.3,
+//     description: 'Slim-fitting style',
+//     category: "men's clothing",
+//     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+//     rating: {
+//       rate: 4.1,
+//       count: 259,
+//     },
+//   },
+//   {
+//     id: 4,
+//     title: 'Mens Casual Slim Fit',
+//     price: 15.99,
+//     description: 'The color could be slightly',
+//     category: "men's clothing",
+//     image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png',
+//     rating: {
+//       rate: 2.1,
+//       count: 430,
+//     },
+//   },
+// ])
+
+const productList = ref([])
+
+import axios from 'axios'
+
 
 // 快速計算陣列數量
-const listCount = computed(()=>{
+const listCount = computed(() => {
   return productList.value.length
 })
 
 // 判斷有無資料
-const noData = computed(()=>{
+const noData = computed(() => {
   return productList.length === 0
 })
 
 // onMounted()
 // debugger
-onMounted(()=>{
+onMounted(() => {
   // debugger
+  axios
+    // 從哪裡取得資料
+    .get('https://fakestoreapi.com/products')
+    // 然後回傳什麼值
+    .then((response) => {
+      // handle success
+      // 用productList去接回傳值
+      productList.value = response.data
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+    .finally(() => {
+      // always executed
+    });
+
 })
 </script>
 
 <template>
   <h2 class="main-title">商品列表</h2>
-  <p>商品總數:{{listCount}}</p>
+  <p>商品總數:{{ listCount }}</p>
   <div v-if="noData">沒有資料</div>
   <div v-else>
     <article class="card" v-for="product in productList">
@@ -116,5 +138,6 @@ body {
 
 .card:hover {
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
 }
 </style>
