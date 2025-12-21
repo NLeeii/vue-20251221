f8360221b9698822f1e97ec21cb870e0d483b481
NLeeii <niniabc920405@gmail.com>
2025-12-21 10:28:01 +0800

    試推

--- src/views/productView.vue
+++ src/views/productView.vue
@@ -6,7 +6,7 @@ const list = ref(['aaa', 'bbb', 'ccc'])
 
 <template>
   <main>
-    <h1>Product</h1>
+    <h1>Product-頁面切換</h1>
     <button v-for="page in pageCount">第{{ page }}頁</button>
     <hr>
     <div v-for="item in list">{{ item }}</div>
